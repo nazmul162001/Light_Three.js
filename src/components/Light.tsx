8dd6a0cabf0bf2e07a89576a265461b32f9e9121
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-03-18 23:27:52 +0600

    rectArea light

--- src/components/Light.tsx
+++ src/components/Light.tsx
@@ -40,6 +40,11 @@ const Light = () => {
     pointLight.position.set(1, -0.5, 1)
     scene.add(pointLight)
 
+    // Rect Area light *** Important ***
+    const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1)
+    rectAreaLight.position.set(-1.5, 0, 1.5)
+    rectAreaLight.lookAt(new THREE.Vector3())
+    scene.add(rectAreaLight)
     // Helper
 
     //Debug
