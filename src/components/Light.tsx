7b3833347fa382d1007cce39afde58910daed8b8
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-03-18 22:44:23 +0600

    install three, lil-gui & setUp three.js component

--- src/components/Light.tsx
+++ src/components/Light.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'lil-gui'
+import { HemisphereLight } from 'three'
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper'
 
 const Light = () => {
   return (
-    <div>Light</div>
+    <section>
+        <canvas className='webgl'>
+                
+        </canvas>
+    </section>
   )
 }
 
